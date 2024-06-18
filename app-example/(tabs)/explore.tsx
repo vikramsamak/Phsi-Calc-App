826a3eb7855a4711041c842028e9e2ef5b70ef72
vikramsamak <vikramsamak02@gmail.com>
2024-06-18 20:26:49 +0530

    Initial setup

---
+++ app-example/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function TabTwoScreen() {
+  return (
+    <ThemedView>
+      <ThemedText>Explore</ThemedText>
+    </ThemedView>
+  );
+}
---
+++ app-example/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ThemedView>
+      <ThemedText>Home</ThemedText>
+    </ThemedView>
+  );
+}
