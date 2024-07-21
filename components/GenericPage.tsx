4cbbdf0f6986066cd6c6ce414c3834481ed960f3
vikramsamak <vikramsamak02@gmail.com>
2024-07-21 23:00:47 +0530

    Imports corrected.

--- components/GenericPage.tsx
+++ components/GenericPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Heading, ScrollView, Text, VStack } from "native-base";
 import GenericForm from "./GenericForm";
 import GenericFormCard from "./GenericFormCard";
-import { GenericPageProps } from "@/constants/InterFaces";
+import { GenericPageProps } from "@/types/InterFaces";
 
 export default function GenericPage({
   heading,
