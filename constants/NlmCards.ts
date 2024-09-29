6f7f622b0736fdbf2acb1858d847addeec62cdc5
vikramsamak <vikramsamak02@gmail.com>
2024-09-29 14:13:29 +0530

    Added validation schemas fro nlm forms.

--- constants/NlmCards.ts
+++ constants/NlmCards.ts
@@ -1,30 +1,51 @@
 import { getapiResponse } from "@/helpers/GetapiResult";
 import { GenericCard } from "../types/InterFaces";
+import {
+  force_schema,
+  momentum_schema,
+  recoil_velocity_schema,
+} from "@/schemas/NlmSchemas";
 
 const force: GenericCard = {
   cardTitle: "Force",
   formfields: [
-    { label: "Mass", placeholder: "Enter Mass", inputType: "number-pad" },
+    {
+      label: "Mass",
+      placeholder: "Enter Mass",
+      type: "number",
+      inputType: "number-pad",
+    },
     {
       label: "Acceleration",
       placeholder: "Enter Acceleration",
+      type: "number",
       inputType: "number-pad",
     },
   ],
-  apiFunction: (data: object) => getapiResponse("/nlm/force", data),
+  validation_schema: force_schema,
+  apiFunction: (data: typeof force_schema) =>
+    getapiResponse("/nlm/force", data),
 };
 
 const momentum: GenericCard = {
   cardTitle: "Momentum",
   formfields: [
-    { label: "Mass", placeholder: "Enter Mass", inputType: "number-pad" },
+    {
+      label: "Mass",
+      placeholder: "Enter Mass",
+      type: "number",
+      inputType: "number-pad",
+    },
     {
       label: "Velocity",
       placeholder: "Enter Velocity",
+      type: "number",
       inputType: "number-pad",
     },
   ],
-  apiFunction: (data: object) => getapiResponse("/nlm/momentum", data),
+  validation_schema: momentum_schema,
+  apiFunction: (data: typeof momentum_schema) =>
+    getapiResponse("/nlm/momentum", data),
 };
 
 const recoil_velocity: GenericCard = {
@@ -33,36 +54,25 @@ const recoil_velocity: GenericCard = {
     {
       label: "Mass of Bullet",
       placeholder: "Enter Mass of bullet",
+      type: "number",
       inputType: "number-pad",
     },
     {
       label: "Initial Velocity",
       placeholder: "Enter Initial Velocity",
+      type: "number",
       inputType: "number-pad",
     },
     {
       label: "Mass of Gun",
       placeholder: "Enter Mass of Gun",
+      type: "number",
       inputType: "number-pad",
     },
   ],
-  apiFunction: (data: object) => getapiResponse("/nlm/recoil_velocity", data),
+  validation_schema: recoil_velocity_schema,
+  apiFunction: (data: typeof recoil_velocity_schema) =>
+    getapiResponse("/nlm/recoil_velocity", data),
 };
 
-export const nlmCards: GenericCard[] = [
-  {
-    cardTitle: force.cardTitle,
-    formfields: force.formfields,
-    apiFunction: force.apiFunction,
-  },
-  {
-    cardTitle: momentum.cardTitle,
-    formfields: momentum.formfields,
-    apiFunction: momentum.apiFunction,
-  },
-  {
-    cardTitle: recoil_velocity.cardTitle,
-    formfields: recoil_velocity.formfields,
-    apiFunction: recoil_velocity.apiFunction,
-  },
-];
+export const nlmCards: GenericCard[] = [force, momentum, recoil_velocity];
